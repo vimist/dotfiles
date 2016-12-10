9e5ef7a68add3255e02f0921f8b6fec91a6cd17a
vimist <vimist@users.noreply.github.com>
2016-12-10 20:59:45 +0000

    Updated the link function to create directory path and have optional $2
    Link function now creates the directories for the destination symlink
    and also works with a single argument as long as the destination path is
    the same as the source path, but relative to $HOME instead

--- setup.sh
+++ setup.sh
@@ -9,11 +9,15 @@ backup_suffix='.dotfile_backup'
 
 # Link a file from this repository into the users home directory
 # backups of existing files will be made with a suffix of $backup_suffix
-# $1 The source file, relative to the 'files' directory
+# $1 The source file, relative to the current directory
 # $2 The destination file, relative to $HOME
+#    $1 is used if $2 is not specified, this allows us to pass in a single
+#    argument if $1 should be recreated under $HOME
 function link() {
-    src="$PWD/files/$1"
-    dst="$HOME/$2"
+    src="$PWD/$1"
+    dst="$HOME/${2:-$1}"
+
+    mkdir -p "$(dirname "$dst")"
 
     # If the destination is a symlink
     if [[ -L "$dst" ]]; then
